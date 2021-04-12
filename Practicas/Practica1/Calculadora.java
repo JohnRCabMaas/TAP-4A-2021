dadc18a1cf630ee421503fd58fe57d00cc4e7ad4
Johan Rodrigo Cab Maas <80074591+JohnRCabMaas@users.noreply.github.com>
2021-04-12 14:23:10 -0500

    Practica1

--- Practicas/Practica1/Calculadora.java
+++ Practicas/Practica1/Calculadora.java
@@ -18,8 +18,8 @@ public class Calculadora extends JFrame {
         txtResultado.setEditable(false);
         txtResultado.setFont(new Font("Open Sans", 1, 18));
         txtResultado.setForeground(new Color(51, 51, 51));
-        txtResultado.setText("0123456789");
-        txtResultado.setPreferredSize(new Dimension(500, 50));
+        txtResultado.setText("inicie colocando un numero");
+        txtResultado.setPreferredSize(new Dimension(430, 30));
         txtResultado.addKeyListener(new KeyAdapter() {
             public void keyTyped(KeyEvent evt){
                 txtResultadokeyTyped(evt);
@@ -31,55 +31,55 @@ public class Calculadora extends JFrame {
         btnC.setFont(new Font("Open Sans", 1, 16));
         btnC.addActionListener(evt -> btnCActionPerformed(evt));
 
-        btnUno = new JButton("1");
-        btnUno.setFocusable(false);
-        btnUno.setFont(new Font("Open Sans", 1, 16));
-        btnUno.addActionListener(evt -> btnUnoActionPerformed(evt));
+        btn1 = new JButton("1");
+        btn1.setFocusable(false);
+        btn1.setFont(new Font("Open Sans", 1, 16));
+        btn1.addActionListener(evt -> btnUnoActionPerformed(evt));
 
-        btnDos = new JButton("2");
-        btnDos.setFocusable(false);
-        btnDos.setFont(new Font("Open Sans", 1, 16));
-        btnDos.addActionListener(evt -> btnDosActionPerformed(evt));
+        btn2 = new JButton("2");
+        btn2.setFocusable(false);
+        btn2.setFont(new Font("Open Sans", 1, 16));
+        btn2.addActionListener(evt -> btnDosActionPerformed(evt));
 
-        btnTres = new JButton("3");
-        btnTres.setFocusable(false);
-        btnTres.setFont(new Font("Open Sans", 1, 16));
-        btnTres.addActionListener(evt -> btnTresActionPerformed(evt));
+        btn3 = new JButton("3");
+        btn3.setFocusable(false);
+        btn3.setFont(new Font("Open Sans", 1, 16));
+        btn3.addActionListener(evt -> btnTresActionPerformed(evt));
 
-        btnCuatro = new JButton("4");
-        btnCuatro.setFocusable(false);
-        btnCuatro.setFont(new Font("Open Sans", 1, 16));
-        btnCuatro.addActionListener(evt -> btnCuatrosActionPerformed(evt));
+        btn4 = new JButton("4");
+        btn4.setFocusable(false);
+        btn4.setFont(new Font("Open Sans", 1, 16));
+        btn4.addActionListener(evt -> btnCuatrosActionPerformed(evt));
 
-        btnCinco = new JButton("5");
-        btnCinco.setFocusable(false);
-        btnCinco.setFont(new Font("Open Sans", 1, 16));
-        btnCinco.addActionListener(evt -> btnCincoActionPerformed(evt));
+        btn5 = new JButton("5");
+        btn5.setFocusable(false);
+        btn5.setFont(new Font("Open Sans", 1, 16));
+        btn5.addActionListener(evt -> btnCincoActionPerformed(evt));
 
-        btnSeis = new JButton("6");
-        btnSeis.setFocusable(false);
-        btnSeis.setFont(new Font("Open Sans", 1, 16));
-        btnSeis.addActionListener(evt -> btnSeisActionPerformed(evt));
+        btn6 = new JButton("6");
+        btn6.setFocusable(false);
+        btn6.setFont(new Font("Open Sans", 1, 16));
+        btn6.addActionListener(evt -> btnSeisActionPerformed(evt));
 
-        btnSiete = new JButton("7");
-        btnSiete.setFocusable(false);
-        btnSiete.setFont(new Font("Open Sans", 1, 16));
-        btnSiete.addActionListener(evt -> btnSieteActionPerformed(evt));
+        btn7 = new JButton("7");
+        btn7.setFocusable(false);
+        btn7.setFont(new Font("Open Sans", 1, 16));
+        btn7.addActionListener(evt -> btnSieteActionPerformed(evt));
 
-        btnOcho = new JButton("8");
-        btnOcho.setFocusable(false);
-        btnOcho.setFont(new Font("Open Sans", 1, 16));
-        btnOcho.addActionListener(evt -> btnOchoActionPerformed(evt));
+        btn8 = new JButton("8");
+        btn8.setFocusable(false);
+        btn8.setFont(new Font("Open Sans", 1, 16));
+        btn8.addActionListener(evt -> btnOchoActionPerformed(evt));
 
-        btnNueve = new JButton("9");
-        btnNueve.setFocusable(false);
-        btnNueve.setFont(new Font("Open Sans", 1, 16));
-        btnNueve.addActionListener(evt -> btnNueveActionPerformed(evt));
+        btn9 = new JButton("9");
+        btn9.setFocusable(false);
+        btn9.setFont(new Font("Open Sans", 1, 16));
+        btn9.addActionListener(evt -> btnNueveActionPerformed(evt));
 
-        btnCero = new JButton("0");
-        btnCero.setFocusable(false);
-        btnCero.setFont(new Font("Open Sans", 1, 16));
-        btnCero.addActionListener(evt -> btnCeroActionPerformed(evt));
+        btn0 = new JButton("0");
+        btn0.setFocusable(false);
+        btn0.setFont(new Font("Open Sans", 1, 16));
+        btn0.addActionListener(evt -> btnCeroActionPerformed(evt));
 
         btnSuma = new JButton("+");
         btnSuma.setFocusable(false);
@@ -91,52 +91,52 @@ public class Calculadora extends JFrame {
         btnResta.setFont(new Font("Open Sans", 1, 16));
         btnResta.addActionListener(evt -> btnRestaActionPerformed(evt));
 
-        btnDiv = new JButton("/");
-        btnDiv.setFocusable(false);
-        btnDiv.setFont(new Font("Open Sans", 1, 16));
-        btnDiv.addActionListener(evt -> btnDivActionPerformed(evt));
+        btnDivision = new JButton("/");
+        btnDivision.setFocusable(false);
+        btnDivision.setFont(new Font("Open Sans", 1, 16));
+        btnDivision.addActionListener(evt -> btnDivActionPerformed(evt));
 
-        btnMult = new JButton("*");
-        btnMult.setFocusable(false);
-        btnMult.setFont(new Font("Open Sans", 1, 16));
-        btnMult.addActionListener(vst -> btnMultActionPerformed(vst));
+        btnMultiplicacion = new JButton("*");
+        btnMultiplicacion.setFocusable(false);
+        btnMultiplicacion.setFont(new Font("Open Sans", 1, 16));
+        btnMultiplicacion.addActionListener(vst -> btnMultActionPerformed(vst));
 
-        btnElevado = new JButton("^");
-        btnElevado.setFocusable(false);
-        btnElevado.setFont(new Font("Open Sans", 1, 16));
-        btnElevado.addActionListener(vst -> btnElevadoActionPerformed(vst));
+        btnElevacion = new JButton("^");
+        btnElevacion.setFocusable(false);
+        btnElevacion.setFont(new Font("Open Sans", 1, 16));
+        btnElevacion.addActionListener(vst -> btnElevadoActionPerformed(vst));
 
-        btnIgual = new JButton("=");
-        btnIgual.setFocusable(false);
-        btnIgual.setFont(new Font("Open Sans", 1, 16));
-        btnIgual.addActionListener(evt -> btnIgualActionPerformed(evt));
+        btnSolucion = new JButton("=");
+        btnSolucion.setFocusable(false);
+        btnSolucion.setFont(new Font("Open Sans", 1, 16));
+        btnSolucion.addActionListener(evt -> btnIgualActionPerformed(evt));
 
 
-        barraMenu = new JPanel();
-        barraMenu.add(txtResultado, BorderLayout.WEST);
-        barraMenu.add(btnC, BorderLayout.EAST);
+        Menu = new JPanel();
+        Menu.add(txtResultado, BorderLayout.WEST);
+        Menu.add(btnC, BorderLayout.EAST);
         
         
         contenedorNumeros = new JPanel();
         contenedorNumeros.setLayout(new GridLayout(4,4, 20, 20));
-        contenedorNumeros.add(btnUno);
-        contenedorNumeros.add(btnDos);
-        contenedorNumeros.add(btnTres);
+        contenedorNumeros.add(btn1);
+        contenedorNumeros.add(btn2);
+        contenedorNumeros.add(btn3);
         contenedorNumeros.add(btnSuma);
-        contenedorNumeros.add(btnCuatro);
-        contenedorNumeros.add(btnCinco);
-        contenedorNumeros.add(btnSeis);
+        contenedorNumeros.add(btn4);
+        contenedorNumeros.add(btn5);
+        contenedorNumeros.add(btn6);
         contenedorNumeros.add(btnResta);
-        contenedorNumeros.add(btnSiete);
-        contenedorNumeros.add(btnOcho);
-        contenedorNumeros.add(btnNueve);
-        contenedorNumeros.add(btnDiv);
-        contenedorNumeros.add(btnMult);
-        contenedorNumeros.add(btnCero);
-        contenedorNumeros.add(btnElevado);
-        contenedorNumeros.add(btnIgual);
+        contenedorNumeros.add(btn7);
+        contenedorNumeros.add(btn8);
+        contenedorNumeros.add(btn9);
+        contenedorNumeros.add(btnDivision);
+        contenedorNumeros.add(btnMultiplicacion);
+        contenedorNumeros.add(btn0);
+        contenedorNumeros.add(btnElevacion);
+        contenedorNumeros.add(btnSolucion);
 
-        this.add(barraMenu, BorderLayout.NORTH);
+        this.add(Menu, BorderLayout.NORTH);
         this.add(contenedorNumeros, BorderLayout.CENTER);
 
         //Personalizacion de la ventana
@@ -152,78 +152,78 @@ public class Calculadora extends JFrame {
     }
 
     private void btnUnoActionPerformed(ActionEvent e){
-        txtResultado.setText(txtResultado.getText() + btnUno.getText());
+        txtResultado.setText(txtResultado.getText() + btn1.getText());
     }
 
     private void btnDosActionPerformed(ActionEvent e){
-        txtResultado.setText(txtResultado.getText() + btnDos.getText());
+        txtResultado.setText(txtResultado.getText() + btn2.getText());
     }
 
     private void btnTresActionPerformed(ActionEvent e){
-        txtResultado.setText(txtResultado.getText() + btnTres.getText());
+        txtResultado.setText(txtResultado.getText() + btn3.getText());
     }
 
     private void btnCuatrosActionPerformed(ActionEvent e){
-        txtResultado.setText(txtResultado.getText() + btnCuatro.getText());
+        txtResultado.setText(txtResultado.getText() + btn4.getText());
     }
 
     private void btnCincoActionPerformed(ActionEvent e){
-        txtResultado.setText(txtResultado.getText() + btnCinco.getText());
+        txtResultado.setText(txtResultado.getText() + btn5.getText());
     }
 
     private void btnSeisActionPerformed(ActionEvent e){
-        txtResultado.setText(txtResultado.getText() + btnSeis.getText());
+        txtResultado.setText(txtResultado.getText() + btn6.getText());
     }
 
     private void btnSieteActionPerformed(ActionEvent e){
-        txtResultado.setText(txtResultado.getText() + btnSiete.getText());
+        txtResultado.setText(txtResultado.getText() + btn7.getText());
     }
 
     private void btnOchoActionPerformed(ActionEvent e){
-        txtResultado.setText(txtResultado.getText() + btnOcho.getText());
+        txtResultado.setText(txtResultado.getText() + btn8.getText());
     }
 
     private void btnNueveActionPerformed(ActionEvent e){
-        txtResultado.setText(txtResultado.getText() + btnNueve.getText());
+        txtResultado.setText(txtResultado.getText() + btn9.getText());
     }
 
     private void btnCeroActionPerformed(ActionEvent e){
-        txtResultado.setText(txtResultado.getText() + btnCero.getText());
+        txtResultado.setText(txtResultado.getText() + btn0.getText());
     }
 
     private void btnSumaActionPerformed(ActionEvent e){
-        primerNumero = Integer.parseInt(txtResultado.getText());
+        primero = Integer.parseInt(txtResultado.getText());
         operador = "+";
         txtResultado.setText("");
     }
 
     private void btnRestaActionPerformed(ActionEvent e){
-        primerNumero = Integer.parseInt(txtResultado.getText());
+        primero = Integer.parseInt(txtResultado.getText());
         operador = "-";
         txtResultado.setText("");
     }
 
     private void btnDivActionPerformed(ActionEvent e){
-        primerNumero = Integer.parseInt(txtResultado.getText());
+        primero = Integer.parseInt(txtResultado.getText());
         operador = "/";
         txtResultado.setText("");
     }
     
     private void btnMultActionPerformed(ActionEvent e){
-        primerNumero = Integer.parseInt(txtResultado.getText());
+        primero = Integer.parseInt(txtResultado.getText());
         operador = "*";
         txtResultado.setText("");
     }
 
     private void btnElevadoActionPerformed(ActionEvent e){
-        primerNumero = Integer.parseInt(txtResultado.getText());
+        primero = Integer.parseInt(txtResultado.getText());
         operador = "^";
         txtResultado.setText("");
     }
 
     private void btnIgualActionPerformed(ActionEvent e){
-        segundoNumero = Integer.parseInt(txtResultado.getText());
-        txtResultado.setText(op.opResultado(operador, primerNumero, segundoNumero));
+        segundo = Integer.parseInt(txtResultado.getText());
+        txtResultado.setText(op.opResultado(operador, primero, segundo));
     }
 
     private void txtResultadokeyTyped(KeyEvent evt){
@@ -242,30 +242,30 @@ public class Calculadora extends JFrame {
         });
     }
 
-    // Variables declaration
+    // estas son las variables utilizadas en el programa
     JTextField txtResultado;
     JButton btnC;
-    JButton btnUno;
-    JButton btnDos;
-    JButton btnTres;
-    JButton btnCuatro;
-    JButton btnCinco;
-    JButton btnSeis;
-    JButton btnSiete;
-    JButton btnOcho;
-    JButton btnNueve;
-    JButton btnCero;  
+    JButton btn1;
+    JButton btn2;
+    JButton btn3;
+    JButton btn4;
+    JButton btn5;
+    JButton btn6;
+    JButton btn7;
+    JButton btn8;
+    JButton btn9;
+    JButton btn0;  
     JButton btnSuma;  
     JButton btnResta;
-    JButton btnDiv;
-    JButton btnMult;
-    JButton btnElevado;
-    JButton btnIgual;
-    JPanel barraMenu;
+    JButton btnDivision;
+    JButton btnMultiplicacion;
+    JButton btnElevacion;
+    JButton btnSolucion;
+    JPanel Menu;
     JPanel contenedorNumeros;
-    int primerNumero;
-    int segundoNumero;
+    int primero;
+    int segundo;
     String operador;
-    // End of variables declaration
+    
 
     }
