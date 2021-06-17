9e61b1b6a6f6270fe310c95985788c36d4ad6ed2
Johan Rodrigo Cab Maas <80074591+JohnRCabMaas@users.noreply.github.com>
2021-06-17 18:18:46 -0500

    Practicas_parcial2_kotlin

---
+++ Practicas_Parcial2 en kotlin/Practica_6/app/src/main/java/com/topicosdeprogramacion/practica_6/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.topicosdeprogramacion.practica_6
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import com.topicosdeprogramacion.practica_6.componentes.GameView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_main)
+
+        var actionBar = supportActionBar
+        actionBar?.hide()
+    }
+
+    fun iniciarGame(view: View){
+        val loggGame = Intent(this, JuegoPong:: class.java)
+        startActivity(loggGame)
+    }
+
+}
